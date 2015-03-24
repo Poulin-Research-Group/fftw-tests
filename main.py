4b4cd494df32a4cd12a33657b1bf3119ef15f7bf
Dakota St. Laurent <d.h.stlaurent@gmail.com>
2015-03-24 12:34:19 -0400

    corrected tests

--- main.py
+++ main.py
@@ -11,39 +11,49 @@ M5 = np.random.rand(1024, 1024) + np.random.rand(1024, 1024)*1j
 M6 = np.random.rand(2048, 2048) + np.random.rand(2048, 2048)*1j
 M7 = np.random.rand(4096, 4096) + np.random.rand(4096, 4096)*1j
 M8 = np.random.rand(8192, 8192) + np.random.rand(8192, 8192)*1j
+print 'constructed random matrices'
 
 # matrices for pyfftw
-n1 = pyfftw.n_byte_align_empty((64, 64), 16, 'complex128')
-n1[:, :] = M1[:, :]
-N1 = pyfftw.builders.fft(n1)
+n1_in  = pyfftw.n_byte_align_empty((64, 64), 16, 'complex128')
+n1_out = pyfftw.n_byte_align_empty((64, 64), 16, 'complex128')
+N1 = pyfftw.FFTW(n1_in, n1_out, axes=(0, 1))
+n1_in[:, :] = M1[:, :]
 
-n2 = pyfftw.n_byte_align_empty((128, 128), 16, 'complex128')
-n2[:, :] = M2[:, :]
-N2 = pyfftw.builders.fft(n2)
+n2_in  = pyfftw.n_byte_align_empty((128, 128), 16, 'complex128')
+n2_out = pyfftw.n_byte_align_empty((128, 128), 16, 'complex128')
+N2 = pyfftw.FFTW(n2_in, n2_out, axes=(0, 1))
+n2_in[:, :] = M2[:, :]
 
-n3 = pyfftw.n_byte_align_empty((256, 256), 16, 'complex128')
-n3[:, :] = M3[:, :]
-N3 = pyfftw.builders.fft(n3)
+n3_in  = pyfftw.n_byte_align_empty((256, 256), 16, 'complex128')
+n3_out = pyfftw.n_byte_align_empty((256, 256), 16, 'complex128')
+N3 = pyfftw.FFTW(n3_in, n3_out, axes=(0, 1))
+n3_in[:, :] = M3[:, :]
 
-n4 = pyfftw.n_byte_align_empty((512, 512), 16, 'complex128')
-n4[:, :] = M4[:, :]
-N4 = pyfftw.builders.fft(n4)
+n4_in  = pyfftw.n_byte_align_empty((512, 512), 16, 'complex128')
+n4_out = pyfftw.n_byte_align_empty((512, 512), 16, 'complex128')
+N4 = pyfftw.FFTW(n4_in, n4_out, axes=(0, 1))
+n4_in[:, :] = M4[:, :]
 
-n5 = pyfftw.n_byte_align_empty((1024, 1024), 16, 'complex128')
-n5[:, :] = M5[:, :]
-N5 = pyfftw.builders.fft(n5)
+n5_in  = pyfftw.n_byte_align_empty((1024, 1024), 16, 'complex128')
+n5_out = pyfftw.n_byte_align_empty((1024, 1024), 16, 'complex128')
+N5 = pyfftw.FFTW(n5_in, n5_out, axes=(0, 1))
+n5_in[:, :] = M5[:, :]
 
-n6 = pyfftw.n_byte_align_empty((2048, 2048), 16, 'complex128')
-n6[:, :] = M6[:, :]
-N6 = pyfftw.builders.fft(n6)
+n6_in  = pyfftw.n_byte_align_empty((2048, 2048), 16, 'complex128')
+n6_out = pyfftw.n_byte_align_empty((2048, 2048), 16, 'complex128')
+N6 = pyfftw.FFTW(n6_in, n6_out, axes=(0, 1))
+n6_in[:, :] = M6[:, :]
 
-n7 = pyfftw.n_byte_align_empty((4096, 4096), 16, 'complex128')
-n7[:, :] = M7[:, :]
-N7 = pyfftw.builders.fft(n7)
+n7_in  = pyfftw.n_byte_align_empty((4096, 4096), 16, 'complex128')
+n7_out = pyfftw.n_byte_align_empty((4096, 4096), 16, 'complex128')
+N7 = pyfftw.FFTW(n7_in, n7_out, axes=(0, 1))
+n7_in[:, :] = M7[:, :]
 
-n8 = pyfftw.n_byte_align_empty((8192, 8192), 16, 'complex128')
-n8[:, :] = M8[:, :]
-N8 = pyfftw.builders.fft(n8)
+n8_in  = pyfftw.n_byte_align_empty((8192, 8192), 16, 'complex128')
+n8_out = pyfftw.n_byte_align_empty((8192, 8192), 16, 'complex128')
+N8 = pyfftw.FFTW(n8_in, n8_out, axes=(0, 1))
+n8_in[:, :] = M8[:, :]
+print 'built pyfftw matrices\n'
 
 
 def pyfftw_test(A):
@@ -58,6 +68,10 @@ num = 10
 for M, N in zip([M1, M2, M3, M4, M5, M6, M7, M8], [N1, N2, N3, N4, N5, N6, N7, N8]):
     numpy_timer  = timeit.Timer("numpy_test(M)",  "from __main__ import M, numpy_test")
     pyfftw_timer = timeit.Timer("pyfftw_test(N)", "from __main__ import N, pyfftw_test")
-    print "shape: ", M.shape, num, " trials"
-    print "  ",  numpy_timer.timeit(number=num), "seconds"
-    print "  ", pyfftw_timer.timeit(number=num), "seconds\n"
+    print "shape:", M.shape, num, "trials"
+
+    numpy_time  = numpy_timer.timeit(number=num)
+    pyfftw_time = pyfftw_timer.timeit(number=num)
+    print "  ",  numpy_time,  "seconds"
+    print "  ",  pyfftw_time, "seconds"
+    print "  pyfftw is", (numpy_time/pyfftw_time), "times faster\n"
